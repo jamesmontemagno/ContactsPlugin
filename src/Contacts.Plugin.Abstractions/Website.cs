356ea1cefad83e2652790b5e4ff4f5926d695887
James Montemagno <james.montemagno@gmail.com>
2016-09-13 10:20:14 -0700

    Add contacts source code

---
+++ src/Contacts.Plugin.Abstractions/Website.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Plugin.Contacts.Abstractions
+{
+  public class Website
+  {
+    public string Address
+    {
+      get;
+      set;
+    }
+  }
+}
