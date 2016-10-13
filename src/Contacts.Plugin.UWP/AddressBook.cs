7b129f2b516da39b83931cb40081c5c287b7b5c9
Seyyed Soroosh Hosseinalipour <soorosh_abi@hotmail.com>
2016-10-13 21:37:49 +0330

    Add some basic class for UWP.

---
+++ src/Contacts.Plugin.UWP/AddressBook.cs
@@ -0,0 +1,36 @@
+﻿using Plugin.Contacts.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+using System.Linq.Expressions;
+
+namespace Plugin.Contacts
+{
+    public sealed class AddressBook : IQueryable<Contact>
+    {
+        public Type ElementType => typeof(Contact);
+
+        public Expression Expression => Expression.Constant(this);
+
+        public IQueryProvider Provider
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public IEnumerator<Contact> GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/Contacts.Plugin.UWP/ContactQueryProvider.cs
@@ -0,0 +1,38 @@
+﻿using Plugin.Contacts.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Plugin.Contacts
+{
+    internal sealed class ContactQueryProvider : IQueryProvider
+    {
+        private IEnumerable<Contact> GetContacts()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IQueryable CreateQuery(Expression expression)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IQueryable<TElement> CreateQuery<TElement>(Expression expression)
+        {
+            throw new NotImplementedException();
+        }
+
+        public object Execute(Expression expression)
+        {
+            throw new NotImplementedException();
+        }
+
+        public TResult Execute<TResult>(Expression expression)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
