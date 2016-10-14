050f80bb4a6c6607691d02e8377ed862247532ae
Seyyed Soroosh Hosseinalipour <soorosh_abi@hotmail.com>
2016-10-14 17:01:46 +0330

    Added UWP unit test project.

---
+++ src/Contacts.Plugin.UWP.Tests/UnitTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+
+namespace Contacts.Plugin.UWP.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
