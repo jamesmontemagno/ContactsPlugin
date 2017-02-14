83d3068e9a3e127142a7fb5c06ef68b245a5370a
yogesh.kushwaha <yogesh.kushwaha25@gmail.com>
2017-02-14 16:04:35 +0530

    Removed Automapper dependency

--- src/Contacts.Plugin.UWP/ContactQueryProvider.cs
+++ src/Contacts.Plugin.UWP/ContactQueryProvider.cs
@@ -1,6 +1,7 @@
-﻿using AutoMapper;
+﻿
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Linq.Expressions;
 using System.Reflection;
@@ -63,68 +64,283 @@ namespace Plugin.Contacts
 
 	internal class UWPContactMapToPluginContact
 	{
-		public static IMapper Mapper
-			=> current?.mapper ?? (current = new UWPContactMapToPluginContact()).mapper;
+        public IEnumerable<Abstractions.Contact> Convert(List<Contact> winContacts)
+        {
+            List<Abstractions.Contact> contacts = new List<Abstractions.Contact>();
+            foreach (var winContact in winContacts)
+            {
+                contacts.Add(Convert(winContact));
+            }
+            return contacts.AsEnumerable();
+        }
 
-		private sealed class DefaultMappingProfile : Profile
-		{
-			public DefaultMappingProfile()
-			{
-				CreateMap<ContactAddress, Abstractions.Address>()
-					 .ForMember(x => x.Type, opt => opt.MapFrom(x => (Abstractions.AddressType)
-						 Enum.Parse(typeof(Abstractions.AddressType), x.Kind.ToString(), true)))
-					 .ForMember(x => x.Label, opt => opt.MapFrom(x => x.Kind.ToString()))
-					 .ForMember(x => x.City, opt => opt.MapFrom(x => x.Locality));
+        public Abstractions.Contact Convert(Contact winContact)
+        {
+            Abstractions.Contact contact = new Abstractions.Contact(winContact.Id,winContact.IsAggregate);
+            try
+            {
+                contact.FirstName = winContact.FirstName;
+                contact.MiddleName = winContact.MiddleName;
+                contact.LastName = winContact.LastName;
+                contact.DisplayName = winContact.DisplayName;
+                contact.Nickname = winContact.Nickname;
+                contact.Prefix = winContact.HonorificNamePrefix;
+                contact.Suffix = winContact.HonorificNameSuffix;
+                contact.Phones = new List<Abstractions.Phone>();
+                foreach (var phone in winContact.Phones)
+                {
+                    contact.Phones.Add(Convert(phone));
+                }
+                contact.Addresses = new List<Abstractions.Address>();
+                foreach (var contactAddress in winContact.Addresses)
+                {
+                    contact.Addresses.Add(Convert(contactAddress));
+                }
+                contact.Emails = new List<Abstractions.Email>();
+                foreach (var contactEmail in winContact.Emails)
+                {
+                    contact.Emails.Add(Convert(contactEmail));
+                }
+                contact.InstantMessagingAccounts = new List<Abstractions.InstantMessagingAccount>();
+                foreach (var contactIM in winContact.ConnectedServiceAccounts)
+                {
+                    contact.InstantMessagingAccounts.Add(Convert(contactIM));
+                }
+                contact.Notes = new List<Abstractions.Note>() { new Abstractions.Note { Contents = winContact.Notes } };
+                contact.Organizations = new List<Abstractions.Organization>();
+                foreach (var jobInfo in winContact.JobInfo)
+                {
+                    contact.Organizations.Add(Convert(jobInfo));
+                }
+                contact.Websites = new List<Abstractions.Website>();
+                foreach (var contactWebsite in winContact.Websites)
+                {
+                    contact.Websites.Add(Convert(contactWebsite));
+                }
+                contact.Relationships = new List<Abstractions.Relationship>();
+                foreach (var relationship in winContact.SignificantOthers)
+                {
+                    contact.Relationships.Add(Convert(relationship));
+                }
+            }
+            catch (Exception ex)
+            {
+#if DEBUG
+                Debug.WriteLine(ex.Message);
+#endif
+            }
+            
+            return contact;
+        }
 
-				// TODO: Maybe better map with ContactRelationship
-				CreateMap<ContactSignificantOther, Abstractions.Relationship>()
-					.ForMember(x => x.Type, opt => opt.MapFrom(x => Abstractions.RelationshipType.SignificantOther));
+        #region AddressConverters
 
-				// Website
-				CreateMap<ContactWebsite, Abstractions.Website>()
-					.ForMember(x => x.Address, opt => opt.MapFrom(x => x.Uri.OriginalString));
+        private Abstractions.Address Convert(ContactAddress contactAddress)
+        {
+            Abstractions.Address address = new Abstractions.Address();
+            address.City = contactAddress.Locality;
+            address.Country = contactAddress.Country;
+            address.Label = contactAddress.Description;
+            address.PostalCode = contactAddress.PostalCode;
+            address.Region = contactAddress.Region;
+            address.StreetAddress = contactAddress.StreetAddress;
+            address.Type = Convert(contactAddress.Kind);
+            return address;
+        }
 
-				// Origanization
-				CreateMap<ContactJobInfo, Abstractions.Organization>()
-					.ForMember(x => x.Name, opt => opt.MapFrom(x => x.CompanyName))
-					.ForMember(x => x.ContactTitle, opt => opt.MapFrom(x => x.Title))
-					.ForMember(x => x.Type, opt => opt.MapFrom(x => Abstractions.OrganizationType.Work))
-					.ForMember(x => x.Label, opt => opt.MapFrom(x => "Work"));
+        private Abstractions.AddressType Convert(ContactAddressKind kind)
+        {
+            if (kind == ContactAddressKind.Home)
+                return Abstractions.AddressType.Home;
+            else if (kind == ContactAddressKind.Work)
+                return Abstractions.AddressType.Work;
+            else
+                return Abstractions.AddressType.Other;
+        }
 
-				// Email
-				CreateMap<ContactEmail, Abstractions.Email>()
-					.ForMember(x => x.Type, opt => opt.MapFrom(x => x.Kind.ToEmailType()))
-					// TODO: Is mandatory same value in Type and Label?
-					.ForMember(x => x.Label, opt => opt.MapFrom(x => x.Kind.ToString()))
-					.ForMember(x => x.Address, opt => opt.MapFrom(x => x.Address));
+        #endregion
 
-				// Phone
-				CreateMap<ContactPhone, Abstractions.Phone>()
-					.ForMember(x => x.Type, opt => opt.MapFrom(x => x.Kind.ToPhoneType()))
-					// TODO: Is mandatory same value in Type and Label?
-					.ForMember(x => x.Label, opt => opt.MapFrom(x => x.Kind.ToString()))
-					.ForMember(x => x.Number, opt => opt.MapFrom(x => x.Number));
+        #region EmailConverters
 
-				CreateMap<Contact, Abstractions.Contact>()
-					.ForMember(x => x.Notes, opt => opt.MapFrom(
-						x => new List<Abstractions.Note> { new Abstractions.Note() { Contents = x.Notes } }));
+        private Abstractions.Email Convert(ContactEmail contactEmail)
+        {
+            Abstractions.Email email = new Abstractions.Email();
+            email.Address = contactEmail.Address;
+            email.Label = contactEmail.Description;
+            email.Type = Convert(contactEmail.Kind);
+            return email;
+        }
 
-			}
-		}
+        private Abstractions.EmailType Convert(ContactEmailKind kind)
+        {
+            if (kind == ContactEmailKind.Personal)
+                return Abstractions.EmailType.Home;
+            else if (kind == ContactEmailKind.Work)
+                return Abstractions.EmailType.Work;
+            else
+                return Abstractions.EmailType.Other;
+        }
+        #endregion
 
-		private UWPContactMapToPluginContact()
-		{
-			configuration = new MapperConfiguration(cfg => cfg.AddProfile<DefaultMappingProfile>());
+        private Abstractions.InstantMessagingAccount Convert(ContactConnectedServiceAccount contactIM)
+        {
+            Abstractions.InstantMessagingAccount im = new Abstractions.InstantMessagingAccount();
+            im.Account = contactIM.Id;
+            im.ServiceLabel = contactIM.ServiceName;
+            return im;
+        }
 
-			mapper = configuration.CreateMapper();
-		}
+        private Abstractions.Organization Convert(ContactJobInfo jobInfo)
+        {
+            Abstractions.Organization org = new Abstractions.Organization();
+            org.Name = jobInfo.CompanyName;
+            org.Label = jobInfo.Description;
+            org.ContactTitle = jobInfo.Department;
+            return org;
+        }
 
-		private static UWPContactMapToPluginContact current;
-		private MapperConfiguration configuration;
-		private IMapper mapper;
-	}
+        #region EmailConverters
 
-	internal sealed class ContactQueryProvider : IQueryProvider
+        private Abstractions.Phone Convert(ContactPhone contactPhone)
+        {
+            Abstractions.Phone phone = new Abstractions.Phone();
+            phone.Number = contactPhone.Number;
+            phone.Label = contactPhone.Description;
+            phone.Type = Convert(contactPhone.Kind);
+            return phone;
+        }
+
+        private Abstractions.PhoneType Convert(ContactPhoneKind kind)
+        {
+            Abstractions.PhoneType type = Abstractions.PhoneType.Other;
+            switch (kind)
+            {
+                case ContactPhoneKind.Home:
+                    type = Abstractions.PhoneType.Home;
+                    break;
+                case ContactPhoneKind.Mobile:
+                    type = Abstractions.PhoneType.Mobile;
+                    break;
+                case ContactPhoneKind.Company:
+                case ContactPhoneKind.Work:
+                    type = Abstractions.PhoneType.Work;
+                    break;
+                case ContactPhoneKind.Other:
+                    break;
+                case ContactPhoneKind.Pager:
+                    break;
+                case ContactPhoneKind.BusinessFax:
+                    type = Abstractions.PhoneType.WorkFax;
+                    break;
+                case ContactPhoneKind.HomeFax:
+                    type = Abstractions.PhoneType.HomeFax;
+                    break;
+            }
+            return type;
+        }
+        #endregion
+
+        private Abstractions.Website Convert(ContactWebsite contactWebsite)
+        {
+            Abstractions.Website website = new Abstractions.Website();
+            website.Address = contactWebsite.RawValue; // should be replace with Uri?
+            return website;
+        }
+
+        #region RelationshipConverters
+
+        private Abstractions.Relationship Convert(ContactSignificantOther contactRelationship)
+        {
+            Abstractions.Relationship relationship = new Abstractions.Relationship();
+            relationship.Name = contactRelationship.Name;
+            relationship.Type = Convert(contactRelationship.Relationship);
+            return relationship;
+        }
+
+        private Abstractions.RelationshipType Convert(ContactRelationship relationship)
+        {
+            Abstractions.RelationshipType type = Abstractions.RelationshipType.Other;
+            switch (relationship)
+            {
+                case ContactRelationship.Other:
+                case ContactRelationship.Partner:
+                case ContactRelationship.Sibling:
+                case ContactRelationship.Parent:
+                    type = Abstractions.RelationshipType.Other;
+                    break;
+                case ContactRelationship.Spouse:
+                    type = Abstractions.RelationshipType.SignificantOther;
+                    break;
+                case ContactRelationship.Child:
+                    type = Abstractions.RelationshipType.Child;
+                    break;
+            }
+            return type;
+        }
+        #endregion
+
+        //public static IMapper Mapper
+        //	=> current?.mapper ?? (current = new UWPContactMapToPluginContact()).mapper;
+
+        //private sealed class DefaultMappingProfile : Profile
+        //{
+        //	public DefaultMappingProfile()
+        //	{
+        //		CreateMap<ContactAddress, Abstractions.Address>()
+        //			 .ForMember(x => x.Type, opt => opt.MapFrom(x => (Abstractions.AddressType)
+        //				 Enum.Parse(typeof(Abstractions.AddressType), x.Kind.ToString(), true)))
+        //			 .ForMember(x => x.Label, opt => opt.MapFrom(x => x.Kind.ToString()))
+        //			 .ForMember(x => x.City, opt => opt.MapFrom(x => x.Locality));
+
+        //		// TODO: Maybe better map with ContactRelationship
+        //		CreateMap<ContactSignificantOther, Abstractions.Relationship>()
+        //			.ForMember(x => x.Type, opt => opt.MapFrom(x => Abstractions.RelationshipType.SignificantOther));
+
+        //		// Website
+        //		CreateMap<ContactWebsite, Abstractions.Website>()
+        //			.ForMember(x => x.Address, opt => opt.MapFrom(x => x.Uri.OriginalString));
+
+        //		// Origanization
+        //		CreateMap<ContactJobInfo, Abstractions.Organization>()
+        //			.ForMember(x => x.Name, opt => opt.MapFrom(x => x.CompanyName))
+        //			.ForMember(x => x.ContactTitle, opt => opt.MapFrom(x => x.Title))
+        //			.ForMember(x => x.Type, opt => opt.MapFrom(x => Abstractions.OrganizationType.Work))
+        //			.ForMember(x => x.Label, opt => opt.MapFrom(x => "Work"));
+
+        //		// Email
+        //		CreateMap<ContactEmail, Abstractions.Email>()
+        //			.ForMember(x => x.Type, opt => opt.MapFrom(x => x.Kind.ToEmailType()))
+        //			// TODO: Is mandatory same value in Type and Label?
+        //			.ForMember(x => x.Label, opt => opt.MapFrom(x => x.Kind.ToString()))
+        //			.ForMember(x => x.Address, opt => opt.MapFrom(x => x.Address));
+
+        //		// Phone
+        //		CreateMap<ContactPhone, Abstractions.Phone>()
+        //			.ForMember(x => x.Type, opt => opt.MapFrom(x => x.Kind.ToPhoneType()))
+        //			// TODO: Is mandatory same value in Type and Label?
+        //			.ForMember(x => x.Label, opt => opt.MapFrom(x => x.Kind.ToString()))
+        //			.ForMember(x => x.Number, opt => opt.MapFrom(x => x.Number));
+
+        //		CreateMap<Contact, Abstractions.Contact>()
+        //			.ForMember(x => x.Notes, opt => opt.MapFrom(
+        //				x => new List<Abstractions.Note> { new Abstractions.Note() { Contents = x.Notes } }));
+
+        //	}
+        //}
+
+        //private UWPContactMapToPluginContact()
+        //{
+        //	configuration = new MapperConfiguration(cfg => cfg.AddProfile<DefaultMappingProfile>());
+
+        //	mapper = configuration.CreateMapper();
+        //}
+
+        //private static UWPContactMapToPluginContact current;
+        //private MapperConfiguration configuration;
+        //private IMapper mapper;
+    }
+
+    internal sealed class ContactQueryProvider : IQueryProvider
 	{
 		public IQueryable CreateQuery(Expression expression)
 		{
@@ -170,8 +386,9 @@ namespace Plugin.Contacts
 			foreach (Contact contact in contacts)
 				mutableContacts.Add(contact);
 
-			return UWPContactMapToPluginContact.Mapper.Map<List<Contact>, IEnumerable<Abstractions.Contact>>(mutableContacts);
-		}
+            //return UWPContactMapToPluginContact.Mapper.Map<List<Contact>, IEnumerable<Abstractions.Contact>>(mutableContacts);
+            return new UWPContactMapToPluginContact().Convert(mutableContacts);
+        }
 
 		private Expression ReplaceQueryable(Expression expression, object value)
 		{
--- src/Contacts.Plugin.UWP/ContactsImplementation.cs
+++ src/Contacts.Plugin.UWP/ContactsImplementation.cs
@@ -2,6 +2,7 @@
 using Plugin.Permissions;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
@@ -23,14 +24,17 @@ namespace Plugin.Contacts
 			var status = await CrossPermissions.Current.CheckPermissionStatusAsync(Permissions.Abstractions.Permission.Contacts).ConfigureAwait(false);
 			if (status != Permissions.Abstractions.PermissionStatus.Granted)
 			{
-				Console.WriteLine("Currently does not have Contacts permissions, requesting permissions");
-
-				var request = await CrossPermissions.Current.RequestPermissionsAsync(Permissions.Abstractions.Permission.Contacts);
+#if DEBUG
+                Debug.WriteLine("Currently does not have Contacts permissions, requesting permissions");
+#endif
+                var request = await CrossPermissions.Current.RequestPermissionsAsync(Permissions.Abstractions.Permission.Contacts);
 
 				if (request[Permissions.Abstractions.Permission.Contacts] != Permissions.Abstractions.PermissionStatus.Granted)
 				{
-					Console.WriteLine("Contacts permission denied, can not get positions async.");
-					return false;
+#if DEBUG
+                    Debug.WriteLine("Contacts permission denied, can not get positions async.");
+#endif
+                    return false;
 				}
 			}
 
